c70a5e66ad90e67c43ce420eb3a0ade6e5c5c43b
Rong0912 <sun21907991@dingtalk.com>
2020-06-05 17:13:19 +0800

    优化前台模块

--- quark-portal/src/main/java/com/quark/porent/utils/StaticPeram.java
+++ quark-portal/src/main/java/com/quark/porent/utils/StaticPeram.java
@@ -10,14 +10,14 @@ public class StaticPeram {
     final static String defaultReadTimeout = "sun.net.client.defaultReadTimeout";
     final static String Timeout = "10000";
     // 初始化ascClient需要的几个参数
-    final static String product = "Dysmsapi";// 短信API产品名称（短信产品名固定，无需修改）
-    final static String domain = "dysmsapi.aliyuncs.com";// 短信API产品域名（接口地址固定，无需修改）
+    final static String product = "";// 短信API产品名称（短信产品名固定，无需修改）
+    final static String domain = "";// 短信API产品域名（接口地址固定，无需修改）
     // 替换成你的AK (产品密)
-    final static String accessKeyId = "LTAI4Fs2fG86w41cjWtxcU7n";// 你的accessKeyId,填你自己的 上文配置所得  自行配置
-    final static String accessKeySecret = "eeLNv24PHtHzuOJS9gM9aSAoyzJR5c";// 你的accessKeySecret,填你自己的 上文配置所得 自行配置
+    final static String accessKeyId = "";// 你的accessKeyId,填你自己的 上文配置所得  自行配置
+    final static String accessKeySecret = "";// 你的accessKeySecret,填你自己的 上文配置所得 自行配置
     // 必填:短信签名-可在短信控制台中找到
-    final static String SignName = "花椒社区网站";// 阿里云配置你自己的短信签名填入
+    final static String SignName = "";// 阿里云配置你自己的短信签名填入
     // 必填:短信模板-可在短信控制台中找到
-    final static String TemplateCode = "SMS_182545330"; // 阿里云配置你自己的短信模板填入
+    final static String TemplateCode = ""; // 阿里云配置你自己的短信模板填入
 
 }
